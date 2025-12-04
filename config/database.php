13eebd09273443a46bc96f1399166cdba310cc05
PeterMIlanezSilva <petermilanezsilva@gmail.com>
2025-12-03 21:42:51 -0300

    Definição do charset para evitar problemas com acentuação

--- config/database.php
+++ config/database.php
@@ -16,4 +16,6 @@ $conn = new mysqli($host, $usuario, $senha, $banco);
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
+// Define o charset para evitar problemas com acentuação
+$conn->set_charset("utf8mb4");
 ?>
